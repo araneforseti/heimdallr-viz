315384fcff9df39f76fb2154940b864f8527746b
Sarah <sarahforseti@gmail.com>
2018-08-08 00:20:42 -0400

    Compares visual differences for one

--- lib/heimdallr-viz.rb
+++ lib/heimdallr-viz.rb
@@ -10,12 +10,17 @@ module HeimdallrViz
       self.driver = driver
     end
 
-    def check_visual(element_selector:)
+    def check_visual(element_selector:, prior_image:)
       Dir.mkdir 'viz_temp' unless File.exist? 'viz_temp'
       driver.save_screenshot('viz_temp/screen.png')
       image = Magick::Image.read('viz_temp/screen.png')[0]
       element = driver.find_element(element_selector)
-      try_element(element, image)
+      new_image = try_element(element, image)
+      unless same_image(new_image, prior_image).zero?
+        create_comparison_map(new_image, prior_image)
+        return false
+      end
+      true
     end
 
     def try_element(element, image)
@@ -25,8 +30,9 @@ module HeimdallrViz
       x = location.x * 2
       y = location.y * 2
 
-      crop_image(image: image, x: x, y: y, width: width, height: height)
       highlight(image: image, x: x, y: y, width: width, height: height)
+      crop_image(image: image, x: x, y: y, width: width, height: height)
+      'viz_temp/cropped.png'
     end
 
     def crop_image(image:, x:, y:, width:, height:)
@@ -40,6 +46,22 @@ module HeimdallrViz
       end
       highlighted = image.composite(area, x, y, Magick::SoftLightCompositeOp)
       highlighted.write('viz_temp/areas_of_interest.png')
+    end
+
+    def same_image(new_image_file, prior_image_file)
+      new_image = Magick::Image.read(new_image_file)
+      prior_image = Magick::Image.read(prior_image_file)
+      new_image <=> prior_image
+    end
+
+    def create_comparison_map(new_image_file, prior_image_file)
+      new_image = Magick::Image.read(new_image_file)[0]
+      prior_image = Magick::Image.read(prior_image_file)[0]
+      difference = prior_image.composite(new_image,
+                                         0,
+                                         0,
+                                         Magick::DifferenceCompositeOp)
+      difference.write('viz_temp/difference.png')
     end
   end
 end
